c7b8b30520891110abb9ec17a1e1ca23ee6a78d6
RosiePuddles <rosiegbartlett@gmail.com>
2021-02-03 15:08:53 +0000

    m

--- main.py
+++ main.py
@@ -67,15 +67,31 @@ def sort(e):
 
 
 L = []
-lengths = [4]
+lengths = [4, 5, 6]
+# n = 2
+k = 4
+iterations = 1000
 
-for k in lengths:
+t_ = time()
+for n in lengths:
     L.append([])
     s0 = []
-    n = 2
     for i in range(1, k + 2):
-        s0.extend([list(n) for n in list(combinations(range(k + 1), i))])
-    for _ in range(10):
+        s0.extend([list(a) for a in list(combinations(range(k + 1), i))])
+    XOR_Combos = []
+    for i in range(2, n + 1):
+        XOR_Combos.append(list(combinations(range(i), 2)))
+
+    included = []
+    for i in range(len(XOR_Combos)):
+        XOR_Combos[i] = [t for t in XOR_Combos[i] if t not in included]
+        included.extend(XOR_Combos[i])
+
+    del included
+
+    positionMix = list(combinations(range(n), 2))
+    XOR_Combos = [[positionMix.index(t) for t in p] for p in XOR_Combos]
+    for _ in range(iterations):
         t0 = time()
         fs = [randint(0, 2 ** (2 * k) - 1)]
         for _ in range(n - 1):
@@ -87,7 +103,7 @@ for k in lengths:
 
         s0 = []
         for i in range(1, k + 2):
-            s0.extend([list(n) for n in list(combinations(range(k + 1), i))])
+            s0.extend([list(a) for a in list(combinations(range(k + 1), i))])
 
         possible = []
         for i in range(n):
@@ -98,20 +114,6 @@ for k in lengths:
         for i in range(2 * n - 1):
             possible[i].child = possible[i + 1]
         possible[-1].child = inf
-
-        XOR_Combos = []
-        for i in range(2, n + 1):
-            XOR_Combos.append(list(combinations(range(i), 2)))
-
-        included = []
-        for i in range(len(XOR_Combos)):
-            XOR_Combos[i] = [t for t in XOR_Combos[i] if t not in included]
-            included.extend(XOR_Combos[i])
-
-        del included
-
-        positionMix = list(combinations(range(n), 2))
-        XOR_Combos = [[positionMix.index(t) for t in p] for p in XOR_Combos]
 
         possible[0].iterate(0)
 
@@ -125,32 +127,64 @@ for k in lengths:
 
     # print(L[-1])
 
-with open('test.txt', 'w') as f:
-    f.write("    " + "S".ljust(19) + "Delta B_s".ljust(23) + "Delta B".ljust(23) + "R_s".ljust(23) + "R".ljust(23) + "\n")
-    f.write('EM1' + "\n")
-    n = 3
-    for i in range(len(lengths)):
-        l = lengths[i]
-        delta = (n * l) / (l - 1)
-        S = [n < delta for n in L[i]]
-        allSuccess = [d for d, s in zip(L[i], S) if s]
-        p = [f'{2 * l} - {sum(S)}/{len(S)}={sum(S) / len(S)}' if len(S) != 0 else 'N/A',
-             f'{sum([i - n * l for i in allSuccess]) / len(allSuccess)}' if len(allSuccess) != 0 else 'N/A',
-             f'{sum([i - n * l for i in L[i]]) / len(L[i])}',
-             f'{round(sum([(i * log2(l) + n * l) / (2 * n * l) for i in allSuccess]) / len(allSuccess), 5)}' if len(
-                 allSuccess) != 0 else 'N/A',
-             f'{round(sum([(i * log2(l) + n * l) / (2 * n * l) for i in L[i]]) / len(L[i]), 5)}']
-        f.write(" | ".join([i.ljust(20) for i in p]) + "\n")
-    f.write('EM2' + "\n")
-    for i in range(len(L)):
-        l = lengths[i]
-        delta = (2 * n * l) / (l - 1)
-        S = [n < delta for n in L[i]]
-        allSuccess = [d for d, s in zip(L[i], S) if s]
-        p = [f'{2 * l} - {sum(S)}/{len(S)}={sum(S) / len(S)}' if len(S) != 0 else 'N/A',
-             f'{sum([i - n * l for i in allSuccess]) / len(allSuccess)}' if len(allSuccess) != 0 else 'N/A',
-             f'{sum([i - n * l for i in L[i]]) / len(L[i])}',
-             f'{round(sum([(i * log2(l)) / (2 * n * l) for i in allSuccess]) / len(allSuccess), 5)}' if len(
-                 allSuccess) != 0 else 'N/A',
-             f'{round(sum([(i * log2(l)) / (2 * n * l) for i in L[i]]) / len(L[i]), 5)}']
-        f.write(" | ".join([i.ljust(20) for i in p]) + "\n")
+print(f'Time - {time() - t_}')
+
+print("    " + "S".ljust(19) + "Delta B_s".ljust(23) + "Delta B".ljust(23) + "R_s".ljust(23) + "R".ljust(23))
+print("EM1")
+for i in range(len(lengths)):
+    n = lengths[i]
+    delta = (n * k) / (log2(k) - 1)
+    all = [sum([len(a) for a in p[:n]]) for p in L[i]]
+    S = [p < delta for p in all]
+    allSuccess = [d for d, s in zip(all, S) if s]
+    p = [f'{n} - {sum(S)}/{len(S)}={sum(S) / len(S)}' if len(S) != 0 else 'N/A',
+         f'{sum([a - n * k for a in allSuccess]) / len(allSuccess)}' if len(allSuccess) != 0 else 'N/A',
+         f'{sum([a - n * k for a in all]) / len(all)}',
+         f'{round(sum([(i * log2(k) + n * k) / (2 * n * k) for i in allSuccess]) / len(allSuccess), 5)}' if len(
+             allSuccess) != 0 else 'N/A',
+         f'{round(sum([(i * log2(k) + n * k) / (2 * n * k) for i in all]) / len(all), 5)}']
+    print(" | ".join([i.ljust(20) for i in p]))
+# print("EM2")
+# for i in range(len(lengths)):
+#     n = lengths[i]
+#     delta = (2 * n * k) / (log2(k) - 1)
+#     all = [sum([len(a) for a in p[:n]]) for p in L[i]]
+#     S = [p < delta for p in all]
+#     allSuccess = [d for d, s in zip(all, S) if s]
+#     p = [f'{n} - {sum(S)}/{len(S)}={sum(S) / len(S)}' if len(S) != 0 else 'N/A',
+#          f'{sum([a - n * k for a in allSuccess]) / len(allSuccess)}' if len(allSuccess) != 0 else 'N/A',
+#          f'{sum([a - n * k for a in all]) / len(all)}',
+#          f'{round(sum([(i * log2(k) + n * k) / (2 * n * k) for i in allSuccess]) / len(allSuccess), 5)}' if len(
+#              allSuccess) != 0 else 'N/A',
+#          f'{round(sum([(i * log2(k) + n * k) / (2 * n * k) for i in all]) / len(all), 5)}']
+#     print(" | ".join([i.ljust(20) for i in p]))
+
+# with open('test.txt', 'w') as f:
+#     f.write("    " + "S".ljust(19) + "Delta B_s".ljust(23) + "Delta B".ljust(23) + "R_s".ljust(23) + "R".ljust(23) + "\n")
+#     f.write('EM1' + "\n")
+#     n = 3
+#     for i in range(len(lengths)):
+#         l = lengths[i]
+#         delta = (n * l) / (l - 1)
+#         S = [n < delta for n in L[i]]
+#         allSuccess = [d for d, s in zip(L[i], S) if s]
+#         p = [f'{2 * l} - {sum(S)}/{len(S)}={sum(S) / len(S)}' if len(S) != 0 else 'N/A',
+#              f'{sum([i - n * l for i in allSuccess]) / len(allSuccess)}' if len(allSuccess) != 0 else 'N/A',
+#              f'{sum([i - n * l for i in L[i]]) / len(L[i])}',
+#              f'{round(sum([(i * log2(l) + n * l) / (2 * n * l) for i in allSuccess]) / len(allSuccess), 5)}' if len(
+#                  allSuccess) != 0 else 'N/A',
+#              f'{round(sum([(i * log2(l) + n * l) / (2 * n * l) for i in L[i]]) / len(L[i]), 5)}']
+#         f.write(" | ".join([i.ljust(20) for i in p]) + "\n")
+#     f.write('EM2' + "\n")
+#     for i in range(len(L)):
+#         l = lengths[i]
+#         delta = (2 * n * l) / (l - 1)
+#         S = [n < delta for n in L[i]]
+#         allSuccess = [d for d, s in zip(L[i], S) if s]
+#         p = [f'{2 * l} - {sum(S)}/{len(S)}={sum(S) / len(S)}' if len(S) != 0 else 'N/A',
+#              f'{sum([i - n * l for i in allSuccess]) / len(allSuccess)}' if len(allSuccess) != 0 else 'N/A',
+#              f'{sum([i - n * l for i in L[i]]) / len(L[i])}',
+#              f'{round(sum([(i * log2(l)) / (2 * n * l) for i in allSuccess]) / len(allSuccess), 5)}' if len(
+#                  allSuccess) != 0 else 'N/A',
+#              f'{round(sum([(i * log2(l)) / (2 * n * l) for i in L[i]]) / len(L[i]), 5)}']
+#         f.write(" | ".join([i.ljust(20) for i in p]) + "\n")
