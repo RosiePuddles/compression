38a9f568723d4352e5f9a2d49afb8bea5852f8cd
RosiePuddles <rosiegbartlett@gmail.com>
2021-02-10 12:55:45 +0000

    changed XOR checks to lookup list, which turns out was VERY useful

--- main.py
+++ main.py
@@ -17,34 +17,54 @@ class Item:
             for i_ in self.iterable:
                 self.currentIteration = i_
                 curLength = sum([len(p.currentIteration) for p in possible[:iteration + 1]])
+                # Length check
                 if curLength < possible[-1].child:
                     self.child.iterate(iteration + 1)
+                else:
+                    break
         elif iteration > n:
-            # Key not first
+            # Key, not first
             for i_ in self.iterable:
                 self.currentIteration = i_
                 # XOR checks
+                XOR_checks[iteration - n] = XORsum(self.currentIteration, possible[iteration - n].currentIteration)
                 yes = True
                 for a in XOR_Combos[iteration - n - 1]:
-                    firstPair = [b.currentIteration for b in
-                                 [possible[positionMix[a][0] + n], possible[positionMix[a][0]]]]
-                    secondPair = [b.currentIteration for b in
-                                  [possible[positionMix[a][1] + n], possible[positionMix[a][1]]]]
-                    yes *= XORsum(*firstPair) ^ XORsum(*secondPair) == cs[a]
+                    yes *= XOR_checks[positionMix[a][0]] ^ XOR_checks[positionMix[a][1]] == cs[a]
                 if yes:
                     if isinstance(self.child, Item):
                         self.child.iterate(iteration + 1)
                     else:
                         self.child = sum([len(p.currentIteration) for p in possible[:n]])
                         [p.save() for p in possible]
-        elif iteration == 0 or iteration == n:
-            # First of it's type either key or BSL
+        elif iteration == 0:
+            # First BSL
             for i_ in self.iterable:
                 self.currentIteration = i_
+                # Length check
+                if len(self.currentIteration) < possible[-1].child:
+                    self.child.iterate(iteration + 1)
+                else:
+                    break
+        else:
+            for i_ in self.iterable:
+                self.currentIteration = i_
+                XOR_checks[0] = XORsum(self.currentIteration, possible[0].currentIteration)
                 self.child.iterate(iteration + 1)
 
     def save(self):
         self.value = self.currentIteration
+
+
+class Res:
+    def __init__(self, time_taken, batch_size, l, BSL_length):
+        self.time_taken = time_taken
+        self.batch_size = batch_size
+        self.l = l
+        self.BSL_length = BSL_length
+
+    def __repr__(self):
+        return f'Time {self.time_taken}s for {self.batch_size} files of length {2 * self.l}. Length of BSL is {self.BSL_length}'
 
 
 def XORsum(k, s):
@@ -62,13 +82,9 @@ def decode(encoded, k, s):
     return encoded ^ XORsum(k, s)
 
 
-def sort(e):
-    return len(e[0][1]) + len(e[1][1]) + len(e[2][1])
-
-
 L = []
-lengths = [4, 5, 6]
-# n = 2
+lengths = [3]
+# n = 2, 3
 k = 4
 iterations = 1000
 
@@ -77,7 +93,7 @@ for n in lengths:
     L.append([])
     s0 = []
     for i in range(1, k + 2):
-        s0.extend([list(a) for a in list(combinations(range(k + 1), i))])
+        s0.extend([list(a) for a in list(combinations(range(k), i))])
     XOR_Combos = []
     for i in range(2, n + 1):
         XOR_Combos.append(list(combinations(range(i), 2)))
@@ -93,56 +109,72 @@ for n in lengths:
     XOR_Combos = [[positionMix.index(t) for t in p] for p in XOR_Combos]
     for _ in range(iterations):
         t0 = time()
-        fs = [randint(0, 2 ** (2 * k) - 1)]
+        maxRand = 2 ** (2 * k) - 1
+        fs = [randint(0, maxRand)]
         for _ in range(n - 1):
-            temp = randint(0, 2 ** (2 * k) - 1)
+            temp = randint(0, maxRand)
             while temp in fs:
-                temp = randint(0, 2 ** (2 * k) - 1)
+                temp = randint(0, maxRand)
             fs.append(temp)
         cs = [fs[i[0]] ^ fs[i[1]] for i in list(combinations(range(n), 2))]
-
-        s0 = []
-        for i in range(1, k + 2):
-            s0.extend([list(a) for a in list(combinations(range(k + 1), i))])
 
         possible = []
         for i in range(n):
             possible.append(Item(s0))
         for i in range(n):
-            possible.append(Item(range(2 ** k)))
+            possible.append(Item(range(2 ** (k + 1) - 1)))
 
         for i in range(2 * n - 1):
             possible[i].child = possible[i + 1]
         possible[-1].child = inf
 
+        XOR_checks = [0] * n
+
         possible[0].iterate(0)
 
-        print(time() - t0)
-        f0 = fs[0] ^ XORsum(possible[n].value, possible[0].value)
-        [print(f'{bitRep(fs[a])} = {bitRep(f0)} ^ XORsum({bitRep(possible[n + a].value)} {possible[a].value})') for a in range(n)]
-        possible = [a.value for a in possible]
-        print('')
+        if isinstance(possible[0].value, list):
+            result = Res(time() - t0, n, k, sum([len(a.value) for a in possible[:n]]))
+            print(result)
+            L[-1].append(result)
 
-        L[-1].append(possible)
-
-    # print(L[-1])
-
-print(f'Time - {time() - t_}')
+timTot = 0
+for i in L:
+    timTot += sum([a.time_taken for a in i])
+print(f'Time total (with processing)    - {time() - t_}\n'
+      f'Time total (without processing) - {timTot}\n'
+      f'Average time per iteration      - {1000 * timTot / iterations}')
 
 print("    " + "S".ljust(19) + "Delta B_s".ljust(23) + "Delta B".ljust(23) + "R_s".ljust(23) + "R".ljust(23))
 print("EM1")
-for i in range(len(lengths)):
-    n = lengths[i]
-    delta = (n * k) / (log2(k) - 1)
-    all = [sum([len(a) for a in p[:n]]) for p in L[i]]
+for i in L:
+    n = i[0].batch_size
+    l = i[0].l
+    delta = (l * (n - 2) - n) / log2(l)
+    all = [p.BSL_length for p in i]
     S = [p < delta for p in all]
     allSuccess = [d for d, s in zip(all, S) if s]
     p = [f'{n} - {sum(S)}/{len(S)}={sum(S) / len(S)}' if len(S) != 0 else 'N/A',
-         f'{sum([a - n * k for a in allSuccess]) / len(allSuccess)}' if len(allSuccess) != 0 else 'N/A',
-         f'{sum([a - n * k for a in all]) / len(all)}',
-         f'{round(sum([(i * log2(k) + n * k) / (2 * n * k) for i in allSuccess]) / len(allSuccess), 5)}' if len(
+         f'{sum([a * log2(l) + 2 * l + n - n * l for a in allSuccess]) / len(allSuccess)}' if len(allSuccess) != 0 else 'N/A',
+         f'{sum([a * log2(l) + 2 * l + n - n * l for a in all]) / len(all)}',
+         f'{round(sum([(a * log2(l) + 2 * l + n * (l + 1)) / (2 * n * l) for a in allSuccess]) / len(allSuccess), 5)}' if len(
              allSuccess) != 0 else 'N/A',
-         f'{round(sum([(i * log2(k) + n * k) / (2 * n * k) for i in all]) / len(all), 5)}']
+         f'{round(sum([(a * log2(l) + 2 * l + n * (l + 1)) / (2 * n * l) for a in all]) / len(all), 5)}']
+    print(" | ".join([i.ljust(20) for i in p]))
+print("EM2")
+for i in L:
+    n = i[0].batch_size
+    l = i[0].l
+    delta = (l * (n - 2)) / log2(l)
+    all = [p.BSL_length for p in i]
+    S = [p < delta for p in all]
+    allSuccess = [d for d, s in zip(all, S) if s]
+    p = [f'{n} - {sum(S)}/{len(S)}={sum(S) / len(S)}' if len(S) != 0 else 'N/A',
+         f'{sum([a * log2(l) + 2 * l + n - 2 * n * l for a in allSuccess]) / len(allSuccess)}' if len(
+             allSuccess) != 0 else 'N/A',
+         f'{sum([a * log2(l) + 2 * l + n - 2 * n * l for a in all]) / len(all)}',
+         f'{round(sum([(a * log2(l) + 2 * l) / (2 * n * l) for a in allSuccess]) / len(allSuccess), 5)}' if len(
+             allSuccess) != 0 else 'N/A',
+         f'{round(sum([(a * log2(l) + 2 * l) / (2 * n * l) for a in all]) / len(all), 5)}']
     print(" | ".join([i.ljust(20) for i in p]))
 # print("EM2")
 # for i in range(len(lengths)):
