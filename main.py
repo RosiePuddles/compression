c15e25e5e507f0e271bde9d8e5fda8dc5569dc6b
RosiePuddles <rosiegbartlett@gmail.com>
2021-02-14 08:34:41 +0000

    Sub 1 second (0.90s)
    Signed-off-by: RosiePuddles <rosiegbartlett@gmail.com>

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ from random import *
 from time import time
 
 
+class Iter_res:
+    def __init__(self, type, keep_on=0):
+        """
+        Iteration result class
+        :param type: True  = No action required continue with iteration
+                     False = Need to break a given number of times
+        """
+        self.type = type
+        self.keep_on = keep_on
+
+    def less(self):
+        self.keep_on -= 1
+        return self
+
+
 class Key:
     def __init__(self, length, file, child=None):
         self.value = 0
@@ -13,21 +28,24 @@ class Key:
         self.current_iteration = 0
         self.child = child
 
-    def iterate(self, iteration, **kwargs):
+    def iterate(self, iteration, **kwargs) -> Iter_res:
         if iteration == n:
             # First key
             for i_ in range(2 ** self.length):
                 self.current_iteration = i_
                 F0C = self.file ^ XORsum(self.current_iteration, possible[0].current_iteration)
                 F0C = [F0C, F0C >> (2 * self.length - 3), F0C % 2]
-                self.child.iterate(iteration + 1, F0C=F0C[0], first=F0C[1], last=F0C[2])
+                res = self.child.iterate(iteration + 1, F0C=F0C[0], first=F0C[1], last=F0C[2])
+                if (not res.type) and res.keep_on > 0:
+                    return res.less()
         else:
             # Not first key, so 'last' and 'first' exist
             last = kwargs['last']
             first = kwargs['first']
             F0C = kwargs['F0C']
-            max_BSL = (self.file ^ F0C).bit_length()
-            if possible[iteration - n].current_iteration[-1] < max_BSL:
+            Dn = self.file ^ F0C
+            max_BSL = (Dn & -Dn).bit_length() - 1
+            if possible[iteration - n].current_iteration[0] <= max_BSL:
                 yes = True
                 if first ^ self.file_split[0]:
                     yes = possible[iteration - n].current_iteration[-1] == self.length - 2
@@ -35,17 +53,23 @@ class Key:
                     yes = possible[iteration - n].current_iteration[0] == 0
                 if yes:
                     low = (first ^ self.file_split[0]) * (1 << (self.length - 1)) + (last ^ self.file_split[1])
-                    for i_ in range(low, 2 ** self.length, (last ^ self.file_split[1]) + 1):
-                        self.current_iteration = i_
-                        self.iteration_checks(iteration, F0C, first, last)
-
-    def iteration_checks(self, iteration, F0C, first, last):
-        if self.file ^ XORsum(self.current_iteration, possible[iteration - n].current_iteration) == F0C:
-            if iteration == 2 * n - 1:
-                self.child = sum([len(p.current_iteration) for p in possible[:n]])
-                [p.save() for p in possible]
-            else:
-                self.child.iterate(iteration + 1, F0C=F0C, first=first, last=last)
+                    if Dn.bit_length() - self.length > possible[iteration - n].current_iteration[-1]:
+                        return Iter_res(False, n - 1)
+                    else:
+                        for i_ in range(low, 2 ** self.length, (last ^ self.file_split[1]) + 1):
+                            self.current_iteration = i_
+                            if self.file ^ XORsum(self.current_iteration,
+                                                  possible[iteration - n].current_iteration) == F0C:
+                                if iteration == 2 * n - 1:
+                                    self.child = sum([len(p.current_iteration) for p in possible[:n]])
+                                    [p.save() for p in possible]
+                                else:
+                                    res = self.child.iterate(iteration + 1, F0C=F0C, first=first, last=last)
+                                    if (not res.type) and res.keep_on > 0:
+                                        return res.less()
+                else:
+                    return Iter_res(False, iteration - n)
+        return Iter_res(True)
 
     def save(self):
         self.value = self.current_iteration
@@ -67,9 +91,12 @@ class BSL:
             previous_length_sum += len(i_)
             if previous_length_sum < possible[-1].child:
                 self.current_iteration = i_
-                self.child.iterate(iteration + 1, previous_length_sum=previous_length_sum)
+                res = self.child.iterate(iteration + 1, previous_length_sum=previous_length_sum)
+                if (not res.type) and res.keep_on > 0:
+                    return res.less()
             else:
                 break
+        return Iter_res(True)
 
     def save(self):
         self.value = self.current_iteration
@@ -86,13 +113,15 @@ class Res:
         self.BSL_length = BSL_length
 
     def __repr__(self):
-        return f'Time {self.time_taken}s for {self.batch_size} files of length {2 * self.l}. Length of BSL is {self.BSL_length}'
+        return f'Time {self.time_taken}s for {self.batch_size} files of length {2 * self.l}. Length of BSL is ' \
+               f'{self.BSL_length}.\n   {self.iterations} were required of a maximum of {self.max_iterations} ' \
+               f'({round(10000 * self.iterations / self.max_iterations, 5)}E-4)'
 
 
-def XORsum(k, s):
+def XORsum(k, s) -> int:
     out = 0
-    for i in s:
-        out ^= k << i
+    for bit_shift in s:
+        out ^= k << bit_shift
     return out
 
 
@@ -116,19 +145,9 @@ for n in lengths:
     s0 = []
     for i in range(1, k + 2):
         s0.extend([list(a) for a in list(combinations(range(k), i))])
-    XOR_Combos = []
-    for i in range(2, n + 1):
-        XOR_Combos.append(list(combinations(range(i), 2)))
 
-    included = []
-    for i in range(len(XOR_Combos)):
-        XOR_Combos[i] = [t for t in XOR_Combos[i] if t not in included]
-        included.extend(XOR_Combos[i])
+    s0_length = len(s0)
 
-    del included
-
-    positionMix = list(combinations(range(n), 2))
-    XOR_Combos = [[positionMix.index(t) for t in p] for p in XOR_Combos]
     for iteration_number in range(iterations):
         t0 = time()
         maxRand = 2 ** (2 * k) - 1
@@ -149,21 +168,21 @@ for n in lengths:
             possible[i].child = possible[i + 1]
         possible[-1].child = inf
 
-        XOR_checks = [0] * n
-
         possible[0].iterate(0, previous_length_sum=0)
 
         if isinstance(possible[0].value, list):
+
             result = Res(time() - t0, n, k, sum([len(a.value) for a in possible[:n]]))
             # print(f'{str(iteration_number).ljust(4)} - {result}')
             L[-1].append(result)
         else:
             print('no')
 
+t_ = time() - t_
 timTot = 0
 for i in L:
     timTot += sum([a.time_taken for a in i])
-print(f'Time total (with processing)    - {time() - t_}\n'
+print(f'Time total (with processing)    - {t_}\n'
       f'Time total (without processing) - {timTot}\n')
 
 print(sum([a.BSL_length for a in L[0]]) / len(L[0]))
